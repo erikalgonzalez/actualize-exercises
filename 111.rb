c1ff68f83bd4ed97ea3cf3c929a90d75ce15d72d
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-25 11:58:39 -0400

    Create 111.rb

---
+++ 111.rb
@@ -0,0 +1,16 @@
+# QUESTION 1
+# Write code to print out the numbers 4, 30, 12, 983 on separate lines. 
+# For this exercise, you are required to use addition, subtraction, multiplication, and division at least once.
+puts 8 / 2
+puts 2 * 15
+puts 13 - 1
+puts 982 + 1
+
+# QUESTION 2
+# Write code using addition, subtraction, multiplication, or division with numbers that causes the computer to crash.
+# Add a comment with a copy of the error message.
+# Add a comment that describes the error message in your own words.
+puts 4 + 4 = 8 # 111.rb:13: syntax error, unexpected '=', expecting end-of-input (SyntaxError) # It's saying '=' should not be there.
+Puts 10 - 5  # 111.rb:14: undefined method 'Puts' for main:Object (NotMethodError) # It's saying it does not know what 'Puts' is. it needs to be 'puts'
+2 * 20 puts # 111.rb:15: syntax error, unexpected local variable or method, expecting end-of-input (SyntaxError) # It's saying that either the multiplication is wrong or the 'puts' is wrong. 
+<puts 16 / 2 #111.rb:16: syntax error, unexpected '<' (SyntaxError) # It's saying that '<' should not be there. 
