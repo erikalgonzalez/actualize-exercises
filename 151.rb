c595960f73c82efab0dfcbc52b1da7125719e3b1
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-27 14:02:28 -0400

    Create 151.rb

---
+++ 151.rb
@@ -0,0 +1,31 @@
+# QUESTION 1
+# Write a method that prints out any message or design that you choose
+# (you can use an ASCII art generator if you want to make it fancy).
+# Then run the method three times to see your message display three times.
+def display_dogsarecool_screen   
+    puts  "______   _______  _______  _______    _______  ______    _______    _______  _______  _______  ___"     
+    puts "|      | |       ||       ||       |  |   _   ||    _ |  |       |  |       ||       ||       ||   |"    
+    puts "|  _    ||   _   ||    ___||  _____|  |  |_|  ||   | ||  |    ___|  |       ||   _   ||   _   ||   |"    
+    puts "| | |   ||  | |  ||   | __ | |_____   |       ||   |_||_ |   |___   |       ||  | |  ||  | |  ||   |"    
+    puts "| |_|   ||  |_|  ||   ||  ||_____  |  |       ||    __  ||    ___|  |      _||  |_|  ||  |_|  ||   |___" 
+    puts "|       ||       ||   |_| | _____| |  |   _   ||   |  | ||   |___   |     |_ |       ||       ||       |"
+    puts "|______| |_______||_______||_______|  |__| |__||___|  |_||_______|  |_______||_______||_______||_______|"
+end     
+if dog_breed = 0.to_s
+    display_dogsarecool_screen
+end
+
+# QUESTION 2
+# When you run the code below, it should print lyrics. 
+# However, right now if you run the code, it doesn't print anything. 
+# Add the appropriate line of code to make the code run properly.
+def display_print_lyrics
+    puts "Now this is a story all about how"
+    puts "My life got flipped turned upside down"
+    puts "And I'd like to take a minute, just sit right there"
+    puts "I'll tell you how I became the prince of a town called Bel-Air"
+end
+if print_lyrics = 0.to_s
+    display_print_lyrics
+end
+  
