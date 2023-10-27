ef497c30306af244551d610d158f1b024e14060a
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-27 16:00:50 -0400

    Create 153.rb

---
+++ 153.rb
@@ -0,0 +1,28 @@
+# QUESTION 1
+# Create a hash called cat to store the cat's name, breed, and age. Then print the hash.
+# Create a Cat class which stores a cat's name, breed, and age. Print an instance of the cat class.
+cat = {"name" => "Lucky", "breed" => "American Long Hair", "age" => 7}
+pp cat
+
+class Cat
+  def initialize(input_name, input_breed, input_age)
+    @name = input_name
+    @breed = input_breed
+    @age = input_age
+  end
+end
+cat = Cat.new("Lucky", "American Long Hair", 7)
+pp cat
+
+# QUESTION 2
+# The following code has several mistakes that cause it to crash and not run properly.
+# Fix the mistakes to make sure the code runs properly.
+class StoreItem
+    def initialize(input_name, input_price)
+    @name = input_name
+    @price = input_price
+    end
+end
+storeitem = StoreItem.new("chair", 100)
+pp storeitem
+   
