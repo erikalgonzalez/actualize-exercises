50b8f6ac170dc96f19a83d2f8298ac04bb7076e6
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-31 15:10:21 -0400

    Create 204.rb

---
+++ 204.rb
@@ -0,0 +1,9 @@
+# QUESTION 1
+# Paste this link in your browser to make a copy of the Career Inventory Google document: 
+#     https://docs.google.com/document/d/1XaPaG6QXSQEajkHE1noa5pMfa27qmegSOY_FY6zBoQA/copy  
+# Fill in the answers for each question in the Career Inventory.
+# Change the sharing settings of the document to "Anyone with the link can view" 
+# Copy the link to your Career Inventory Google document and paste it below.
+
+
+#https://docs.google.com/document/d/107Az0ks3Jl7gkEBU8HT7G5tg4yTYNyorA5n6OCly7t0/edit#heading=h.sp23mc8tmnjz
