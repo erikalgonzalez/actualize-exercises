b9169c4908bab4ed75876bf651ae6c8ffa5688e6
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-31 16:09:10 -0400

    Create 211.rb

---
+++ 211.rb
@@ -0,0 +1,17 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+x = 20
+if x == 10
+    puts 0
+else 
+ puts -1
+end
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# It was easy to solve but I had a little trouble rembering how to start the code. I had to remember what a conditon was first to be able
+# to write the code and solve the problem. 
