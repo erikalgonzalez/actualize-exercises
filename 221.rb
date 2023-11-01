09ef6911269128fa59df700df335580757e703a6
Erika Gonzalez <erikagonzz01@gmail.com>
2023-10-31 20:09:12 -0400

    Add exercise solutions

---
+++ 221.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+#Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+number = 30
+if number < 10
+	puts -1
+	elsif number > 10
+		puts 1
+	else
+		puts 0
+end
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# After doing the practice problem before this it was a lot easier to do from memory but I had to remind myself that elsif goes before else. 
+
