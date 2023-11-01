786f017ea7a5d14bec08dec555b319ce67e4af89
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-01 12:51:42 -0400

    Update 224.rb

--- 224.rb
+++ 224.rb
@@ -71,36 +71,36 @@ banana = 2
 carrot = 3
 
 if apple > carrot
-	if apple > 1
-			p 1
-		else
-			p 2
-	if banana == 2
-			p 3
-		else
-			p 4
-		end
-	end
+  if apple > 1
+    p 1
+  else
+    p 2
+    if banana == 2
+      p 3
+    else
+      p 4
+    end
+  end
 elsif apple == carrot
-	if apple > 1
-			p 5
-		else
-			p 6
-	if banana == 2
-			p 7
-		else
-			p 8
-		end
-	end
+  if apple > 1
+    p 5
+  else
+    p 6
+    if banana == 2
+      p 7
+    else
+      p 8
+    end
+  end
 else
-	if apple > 1
-			p 9
-		else
-			p 10
-		if banana == 2
-			p 11
-		else
-			p 12
-		end
-	end
+  if apple > 1
+    p 9
+  else
+    p 10
+    if banana == 2
+      p 11
+    else
+      p 12
+    end
+  end 
 end
