0f97cb47bdb569e85b642a6b82bed615b186006a
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-01 15:54:47 -0400

    Create 231.rb

---
+++ 231.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+number = 15
+if number < 10
+  puts 9
+elsif number < 20
+  puts 19
+elsif number < 30
+  puts 29
+else 
+  puts -1
+end
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to slowly write the code from memory. I just have to stop double guessing myself and be more confident.
+# I know with a little bit more practice I can write the code easily from memory once I do I will move on to either loops or variables.
