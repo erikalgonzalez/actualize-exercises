d74ee7e990f7b0dbd35c97e0a11da3e9d800dbc7
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-02 19:22:30 -0400

    Create 242.rb

---
+++ 242.rb
@@ -0,0 +1,14 @@
+# QUESTION 1
+# Use a variable to store an array with three different words.
+# Then add one more word to the array and print the array on one line.
+house = ["kitchen", "living room", "dining room"]
+house.push("bedroom")
+pp house
+
+
+# QUESTION 2
+# Use a variable to store a hash with two different words and their definitions.
+# Then add one more word with its definition and print the hash on one line.
+car = {"steering wheel" => "a wheel that a driver rotates in order to steer a vehicle.", "seat belt" => "a belt or strap securing a person to prevent injury."}
+car["engine"] = "a machine with moving parts that coverts power into motion."
+pp car
