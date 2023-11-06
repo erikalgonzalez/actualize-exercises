5d355be09df9e33f783393d6b1ba1017420b810e
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-06 16:45:50 -0500

    Create 311.rb

---
+++ 311.rb
@@ -0,0 +1,23 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Start with an array of numbers and create a new array with each number times 3.
+# For example, [1, 2, 3] becomes [3, 6, 9].
+
+numbers = [5, 10, 20, 25]
+new_numbers = []
+index = 0
+while index < numbers.length
+  new_numbers << numbers[index] * 3
+index += 1
+end 
+p new_numbers
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve the problem by memory but still had a little bit of doubt whether or not I am doing it right. I will continue to practice 
+# until I don't second guess myself.
