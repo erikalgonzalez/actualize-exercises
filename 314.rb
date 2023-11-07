ed3ce3e1a9d96a7a446fccc794793688a313635e
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-07 12:43:10 -0500

    Add exercise solutions

---
+++ 314.rb
@@ -0,0 +1,34 @@
+# Step 1: Read the problem below.
+# Step 2: Decompose the problem into smaller testable problems. DO NOT SOLVE THE PROBLEM YET.
+# Step 3: Solve each problem until you get stuck. YOU DO NOT NEED TO SOLVE THE COMPLETE PROBLEM!
+
+# QUESTION 1
+# Ancay ouyay igurefay histay neoay utoay?
+# Pig Latin is a very sophisticated language in which 
+# each English word is converted as follows: 
+# The first letter of each word is removed from the beginning and added to the end. 
+# Then, the letters 'ay' are added to the very end of the word.
+# For example: hello = ellohay, pingpong = ingpongpay, marmalade = armalademay, etc.
+# Write a program that asks the user to enter a single word 
+# and prints out the Pig Latin version of that word.
+
+puts "Enter a word and I will show you how it looks in Pig Latin."
+def piglatin
+  alphabet = ('a'..'z').to_a
+  vowels = ['a', 'e', 'i', 'o', 'u']
+  constants = alphabet - vowels
+  word = sent.slpit(' ')
+  if vowels.include?(word[0])
+end 
+# def piglatin
+#   a = string.split(" ")
+#   b = a.map {|word| piglatin_word(word)}
+#   b.join(" ")
+# end 
+# def piglatin_word
+#   firstletter = word[0].downcase
+#   if firstletter =="a" || firstletter == "e" || firstletter == "i" || firstletter == "o" || firstletter == "u"
+#     "#{word}ay"
+#   end
+# end
+
