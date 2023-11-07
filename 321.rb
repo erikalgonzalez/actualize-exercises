9a964ab05abefb895093a73fa87590f3be8cb4c9
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-07 14:55:03 -0500

    Update 321.rb

--- 321.rb
+++ 321.rb
@@ -1,3 +1,7 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
 # Start with an array of strings and create a new array with each string's length
 words = ["GutenTag", "AnyoungHaseyo", "Hola", "Bonjour", "Konnichiwa"]
 lengths = []
@@ -7,3 +11,9 @@ while index < words.length
   index = index + 1
 end 
 p lengths
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve this problem from memory. 
