257741b7fdd107bf179a9b8a0cc3694fa73d009b
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-08 17:18:46 -0500

    Create 331.rb

---
+++ 331.rb
@@ -0,0 +1,16 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+# Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+countries = ["Uruguay", "Japan", "Sweden"]
+countries.push("Jamaica")
+pp countries
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# Yes I was able to solve the problem by memory. I think next time I will try a harder problem. 
