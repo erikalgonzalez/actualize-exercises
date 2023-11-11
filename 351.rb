722bb3073b05158406509d1b83f796b5b5a3dd41
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-10 23:24:33 -0500

    Create 351.rb

---
+++ 351.rb
@@ -0,0 +1,20 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+#  Write a Rectangle class with attributes for width and height.
+class Rectangle
+  def initialize (width, height)
+    @width = width
+    @height = height
+  end 
+end 
+rectangle = Rectangle.new(5, 25)
+pp rectangle
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve this problem by memory for the most part. The only thing I had forgotten was the @ inside the def method.
