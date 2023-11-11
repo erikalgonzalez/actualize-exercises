8565a5ca64f34d8fbd7b0802a414c8bf46b23b40
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-10 23:24:25 -0500

    Create 341.rb

---
+++ 341.rb
@@ -0,0 +1,18 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+
+#  Write a method that takes in a number and returns the number times two. Then run the method and print the result.
+def double (number)
+  return number * 2
+end 
+pp double (9)
+
+
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+#  Yes I was able to solve this problem from memory. I will try a harder methods or classes problem.
