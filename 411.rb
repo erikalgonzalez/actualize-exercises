695dfc1d66f386cd0064b0562274664c8e4b762e
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-13 21:08:13 -0500

    Create 411.rb

---
+++ 411.rb
@@ -0,0 +1,23 @@
+# DELIBERATE PRACTICE REFLECTION
+# Paste a deliberate practice question you are currently working on below.
+# Write the code to solve the problem without looking at any notes or other resources.
+
+# Write a Car class with attributes for make, model, year, and color.
+
+class Car
+  def initialize(make, model, year, color)
+    @make = make
+    @model = model
+    @year = year
+    @color = color
+  end
+end
+
+car = Car.new("Honda", "Accord", 2010, "Red")
+pp car
+
+# Were you able to easily solve the problem from memory?
+# If so, describe the next skill you will start to practice tomorrow.
+# If not, describe the part you got stuck on that requires more practice.
+
+# I was able to solve the problem from memory. Next I will move on to practice the classes.
