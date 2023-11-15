149621a0c01f66eb430c7d5100687db140f5a0f1
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-14 22:15:57 -0500

    Update 324.rb

--- 324.rb
+++ 324.rb
@@ -8,5 +8,21 @@
 # an array that removes all duplicates from the original array. 
 # That is, the second array should look like: [4, 6, 1, 2, 8, 3, 7]
 # The one catch: Don't use the "uniq" method available to arrays! (That would do this for you.)
-numbers = [4, 6, 1, 4, 2, 8, 3, 4, 1, 7].to_set.to_a
-pp numbers
+numbers = [4, 6, 1, 4, 2, 8, 3, 4, 1, 7]
+
+# Create a method that prints out the array of numbers.
+def remove_dupes(array)
+  return array
+end 
+
+#  Set up new array.
+response = Array.new
+
+# Create a method to Iterate over the array.
+numbers.each do |number|
+  puts number
+end 
+
+# Append the value of the array.
+response << number unless response.include?(number)
+
