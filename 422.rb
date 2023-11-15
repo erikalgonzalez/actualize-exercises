ce22c65808314af9ad750e06020e8da7b6609159
Erika Gonzalez <erikagonzz01@gmail.com>
2023-11-15 00:33:21 -0500

    Create 422.rb

---
+++ 422.rb
@@ -0,0 +1,8 @@
+# QUESTION 1
+# Write a method that takes in a string and a number n and returns the string repeated n times.
+# Then run the method and print the result.
+def repeat_string(string,n)
+  return string * n
+end 
+result = repeat_string(" Hello ", 5)
+pp result
